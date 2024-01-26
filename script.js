a4cd0751eb7f188491a2c5768e2aeff873cd13d4
Suresh R P <rpsuresh05@gmail.com>
2024-01-25 20:42:10 -0600

    add timer functionality

--- script.js
+++ script.js
@@ -50,9 +50,46 @@ recordBtnCont.addEventListener('click', (e) => {
     if (recordFlag) {
         recorder.start();
         recordBtn.classList.add('scale-record');
+        startTimer();
 
     } else {
         recorder.stop();
         recordBtn.classList.remove('scale-record');
+        stopTimer();
     }
-})
+});
+
+let timerId;
+
+let timer = document.querySelector(".timer");
+let counter = 0;
+
+function startTimer() {
+    timer.style.display = 'block';
+    function displayTimer() {
+        let totalSeconds = counter;
+
+        let hours = Number.parseInt(totalSeconds / 1000);
+        totalSeconds %= 3600;
+
+        let minutes = Number.parseInt(totalSeconds / 60);
+        totalSeconds %= 60;
+
+        let seconds = totalSeconds;
+
+        hours = hours < 10 ? `0${hours}` : hours;
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        seconds = seconds < 10 ? `0${seconds}` : seconds
+
+        timer.innerText = `${hours}:${minutes}:${seconds}`
+        counter++;
+
+    }
+    timerId = setInterval(displayTimer, 1000)
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+    timer.innerText = "00:00:00";
+    timer.style.display = 'none'
+}
